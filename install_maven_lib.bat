29dd47e40c3cd2808665eb4d8724dd7fa6bb3756
junoyoon <junoyoon@nhn.com>
2013-09-05 15:53:54 +0900

    add missing jxbrowser 3.4

--- install_maven_lib.bat
+++ install_maven_lib.bat
@@ -1,2 +1,6 @@
+call mvn install:install-file -Dfile=lib/xmlbeans-2.5.0-p1.jar -DgroupId=org.apache.xmlbeans -DartifactId=xmlbeans -Dversion=2.5.0-p1 -Dpackaging=jar
 call mvn install:install-file -Dfile=lib/jxbrowser-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser -Dversion=4.0-beta -Dpackaging=jar
 call mvn install:install-file -Dfile=lib/jxbrowser-win-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser-win -Dversion=4.0-beta -Dpackaging=jar	
+call mvn install:install-file -Dfile=lib/jxbrowser-mac-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser-mac -Dversion=4.0-beta -Dpackaging=jar
+call mvn install:install-file -Dfile=lib/jxbrowser-linux32-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser-linux32 -Dversion=4.0-beta -Dpackaging=jar
+call mvn install:install-file -Dfile=lib/jxbrowser-linux64-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser-linux64 -Dversion=4.0-beta -Dpackaging=jar
--- install_maven_lib.sh
+++ install_maven_lib.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
-mvn install:install-file -Dfile=lib/jxbrowser-3.4.jar -DgroupId=jxbrowser -DartifactId=jxbrowser -Dversion=3.4 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/xmlbeans-2.5.0-p1.jar -DgroupId=org.apache.xmlbeans -DartifactId=xmlbeans -Dversion=2.5.0-p1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/jniwrap-3.8.4.jar -DgroupId=jxbrowser -DartifactId=jniwrap -Dversion=3.8.4 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/development-1.1.jar -DgroupId=jxbrowser -DartifactId=development -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/runtime-1.1.jar -DgroupId=jxbrowser -DartifactId=runtime -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/engine-gecko-1.1.jar -DgroupId=jxbrowser -DartifactId=engine-gecko -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/engine-gecko15-1.1.jar -DgroupId=jxbrowser -DartifactId=engine-gecko15 -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/engine-webkit-1.1.jar -DgroupId=jxbrowser -DartifactId=engine-webkit -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/engine-ie-1.1.jar -DgroupId=jxbrowser -DartifactId=engine-ie -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/comfyj-2.10.jar -DgroupId=jxbrowser -DartifactId=comfyj -Dversion=2.10 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/winpack-3.8.4.jar -DgroupId=jxbrowser -DartifactId=winpack -Dversion=3.8.4 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/xulrunner15-windows-1.1.jar -DgroupId=jxbrowser -DartifactId=xulrunner15-windows -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/xulrunner-windows-1.1.jar -DgroupId=jxbrowser -DartifactId=xulrunner-windows -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/xulrunner-linux-1.1.jar -DgroupId=jxbrowser -DartifactId=xulrunner-linux -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/xulrunner-linux64-1.1.jar -DgroupId=jxbrowser -DartifactId=xulrunner-linux64 -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/xulrunner-mac-1.1.jar -DgroupId=jxbrowser -DartifactId=xulrunner-mac -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/xulrunner15-mac-1.1.jar -DgroupId=jxbrowser -DartifactId=xulrunner15-mac -Dversion=1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/jxbrowser-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser -Dversion=4.0-beta -Dpackaging=jar
+mvn install:install-file -Dfile=lib/jxbrowser-win-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser-win -Dversion=4.0-beta -Dpackaging=jar	
+mvn install:install-file -Dfile=lib/jxbrowser-mac-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser-mac -Dversion=4.0-beta -Dpackaging=jar
+mvn install:install-file -Dfile=lib/jxbrowser-linux32-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser-linux32 -Dversion=4.0-beta -Dpackaging=jar
+mvn install:install-file -Dfile=lib/jxbrowser-linux64-4.0-beta.jar -DgroupId=jxbrowser -DartifactId=jxbrowser-linux64 -Dversion=4.0-beta -Dpackaging=jar
